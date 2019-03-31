09889af1942d4789301afc100564b3bbbc669d64
koala99 <lilei19891030@qq.com>
2019-03-31 14:30:42 +0800

    add message test

--- app/src/main/java/com/android/demo/MainActivity.kt
+++ app/src/main/java/com/android/demo/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.android.demo
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mHandler = Handler(Handler.Callback { msg ->
+        if (msg.what == 1) {
+
+            Log.e("收到消息", "1")
+        }
+        false
+    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initdata()
+    }
+
+    fun initdata() {
+        send_msg.setOnClickListener {
+            Log.e("发送消息", "1")
+            var mMessage = Message()
+            mMessage.what = 1
+            mHandler.sendMessageDelayed(mMessage, 5000)
+        }
+        dis_msg.setOnClickListener {
+            Log.e("移除消息", "1")
+            mHandler.removeMessages(1)
+        }
     }
 }
